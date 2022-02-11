006d781dbb4d3c3236ee4e67b59b2ce546ac3a1f
othmaneOUKBIL <80427030+othmaneOUKBIL@users.noreply.github.com>
2022-02-12 00:24:40 +0100

    Update ZHIWEI SUN SQUARES.py

--- ZHIWEI SUN SQUARES.py
+++ ZHIWEI SUN SQUARES.py
@@ -1,22 +1,30 @@
-import math
-d=[9,24,144,365,888,2020,12345,23456,24576]
+ """	Goal
+Given a positive integer n, find the number of ordered tuples (a, b, c, d, e) over non-negative integers for which a² + b² + c² + d² = n and b + 3c + 5d = e².
 
-for k in d:
-    n=k
-    l=[]
-    for b in range(0,math.floor(n**0.5)+1):
-        for c in range(0,math.floor(n**0.5)+1):
-            for d in range(0,math.floor(n**0.5)+1):
-                if (b+3*c+5*d)**0.5==math.floor((b+3*c+5*d)**0.5):
-                    l+=[[b,c,d]]
+Note: A conjecture by 孙智伟 (Sūn, Zhìwěi) states that this count is always at least 1.
+
+For example, if n is 9, there are 3 solutions, namely:
+
+(0, 0, 3, 0, 3)
+(1, 0, 2, 2, 4)
+(3, 0, 0, 0, 0)
+"""
+import math
+
+n=int(input())
+l=[]
+for b in range(0,math.floor(n**0.5)+1):
+    for c in range(0,math.floor(n**0.5)+1):
+        for d in range(0,math.floor(n**0.5)+1):
+            if (b+3*c+5*d)**0.5==math.floor((b+3*c+5*d)**0.5):
+                l+=[[b,c,d]]
     s=0
-    i=1
     for j in l:
         for a in range(0,math.floor(n**0.5)+1):
             if a**2+j[0]**2+j[1]**2+j[2]**2==n:
                 s+=1
-        i+=1
-    print("test",k," OK ",s)
+        
+print("the number of ordered tuples (a, b, c, d, e) is : ",s)
 
     
     
